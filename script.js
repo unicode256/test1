0cfa6720bc428ad632fbec53a44b99e618c24246
GLEB <unicode256@yandex.by>
2020-02-08 21:39:56 +0300

    added submenu

---
+++ script.js
@@ -0,0 +1,7 @@
+// $('.hoverable.h2').on('mouseenter', function() {
+//     $('.fade_element').fadeIn(150);
+// });
+
+// $('.hoverable.h2').on('mouseleave', function() {
+//     $('.fade_element').fadeOut(150);
+//переделать через таймер
